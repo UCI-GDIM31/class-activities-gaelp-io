b66a29c7123d406a2133096ad08ecee60434134d
gaelp-io <gandresp20@gmail.com>
2025-10-27 18:51:29 -0700

    Finished part 1 with deer

--- In Class Activities/Assets/W5/Scripts/DeerW5.cs
+++ In Class Activities/Assets/W5/Scripts/DeerW5.cs
@@ -5,3 +5,16 @@ using UnityEngine.AI;
 // Hint: if you don't remember what a class is supposed to look like,
 //      maybe check out CatW5...
 // If you copied the class declaration from CatW5, you'd only need to change one thing...
+
+public class DeerW5 : MonoBehaviour
+{
+    [SerializeField] GameObject _target;
+
+    NavMeshAgent _meshAgent;
+
+    private void Start()
+    {
+        _meshAgent = GetComponent<NavMeshAgent>();
+        _meshAgent.SetDestination(_target.transform.position);
+    }
+}
